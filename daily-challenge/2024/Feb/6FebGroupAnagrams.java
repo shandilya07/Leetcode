e50159b5b690c632c290ebbc58f4013aaa9fe32b
Shandilya <135587831+shandilya07@users.noreply.github.com>
2024-02-06 11:23:52 +0530

    Create 6FebGroupAnagrams.java

---
+++ daily-challenge/2024/Feb/6FebGroupAnagrams.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length == 0) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> res = new HashMap<>();
+        
+        for(String st : strs) {
+            int[] count = new int[26];
+            
+            for(char c : st.toCharArray()) {
+                count[c - 'a']++;
+            }
+            
+            StringBuilder key = new StringBuilder();
+            for(int i = 0; i < 26; i++) {
+                key.append("#");
+                key.append(count[i]);
+            }
+            
+            if(!res.containsKey(key.toString())) {
+                res.put(key.toString(), new ArrayList<>());
+            }
+            res.get(key.toString()).add(st);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
